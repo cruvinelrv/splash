ecced638478d46d70b1ca7ade4cea3ffe73ff9a3
Vinicius Machado <39386478+cruvinelrv@users.noreply.github.com>
2022-01-16 22:05:20 -0300

    primeiro upload

---
+++ lib/splash.dart
@@ -0,0 +1,4 @@
+library splash;
+
+export 'src/splash_module.dart';
+export 'src/presentation/splash_page.dart';
---
+++ lib/src/splash_module.dart
@@ -0,0 +1,12 @@
+import 'package:flutter_modular/flutter_modular.dart';
+import 'package:splash/src/presentation/splash_page.dart';
+
+class SplashModule extends Module {
+  @override
+  List<Bind> get binds => [];
+
+  @override
+  List<ModularRoute> get routes => [
+        ChildRoute('/', child: (context, args) => const SplashPage()),
+      ];
+}
